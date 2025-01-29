46262d0a40cf92bfd5aa7e636bfd0203aea1b1ca
Joey1610 <josephcastellinoprojects@gmail.com>
2025-01-29 18:46:33 +0530

    Text-Preprocessing

---
+++ package.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import wordpunct_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+
+#Stemming
+from nltk.stem import PorterStemmer
+from nltk.stem import RegexpStemmer
+from nltk.stem import SnowballStemmer
+# nltk.download('all')
